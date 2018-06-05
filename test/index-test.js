51e8f8b17be6012bdf816d6c08e46f15d80a6fb2
Sarah Jacob <smjacob91@gmail.com>
2018-06-05 17:37:30 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -9,7 +9,7 @@ it('add(a, b) adds two numbers and returns the result', function() {
   expect(add(a, b)).toEqual(a + b)
 })
 
-xit('subtract(a, b) subtracts b from a and returns the result', function() {
+it('subtract(a, b) subtracts b from a and returns the result', function() {
   expect(subtract(a, b)).toEqual(a - b)
 })
 
