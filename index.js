06a0f30bacac5a4e2860a3c532bcd430ccae71b8
Sarah Jacob <smjacob91@gmail.com>
2018-06-05 17:39:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ function subtract(a, b) {
   return a - b
 }
 
-function multiply (a, b) {
+function multiply(a, b) {
   return a * b
 }
 
-function divide (a, b)
+function divide(a, b) {
+  return a /b
+}
+
