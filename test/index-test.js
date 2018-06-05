f1f6d0be09df5c523a45aab1afd562a9e6ea55c8
Sarah Jacob <smjacob91@gmail.com>
2018-06-05 17:38:36 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -13,11 +13,11 @@ it('subtract(a, b) subtracts b from a and returns the result', function() {
   expect(subtract(a, b)).toEqual(a - b)
 })
 
-xit('multiply(a, b) multiplies two numbers and returns the result', function() {
+it('multiply(a, b) multiplies two numbers and returns the result', function() {
   expect(multiply(a, b)).toEqual(a * b)
 })
 
-xit('divide(a, b) divides a by b and returns the result', function() {
+it('divide(a, b) divides a by b and returns the result', function() {
   expect(divide(a, b)).toEqual(a / b)
 })
 
